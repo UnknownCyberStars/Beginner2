f40be361cca71760af45be9dc51be8101304380b
UnknownCyberStars <1715081283@qq.com>
2026-08-13 21:29:21 +0800

    first push

---
+++ test2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(int a,int b) {
+    cout << a+b << endl;
+}
+int main() {
+    int a=1,b=1;
+    cout << "This is my starting point." << endl;
+    cout << "A starting point like 1, then, ";
+    solve(a,b);
+    cout << "I hope I can find something I truly love." << endl;
+    cout << "Maybe today...";
+}
